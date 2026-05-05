c0f649c277f5174bdad5b49a855b222f927ab5d5
RenanWARZ <renanpilan711@gmail.com>
2026-05-04 22:04:56 -0300

    Criando conexão com o backend

---
+++ frontend/src/service/auth.service.ts
@@ -0,0 +1,22 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthResponse, LoginPayload, UsuarioCreate } from './model/auth.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private http = inject(HttpClient);
+
+  private authUrl = 'http://localhost:8080/auth';
+  private jogadorUrl = 'http://localhost:8080/usuario';
+
+  criarConta(payload: UsuarioCreate): Observable<any> {
+    return this.http.post<any>(`${this.jogadorUrl}/cadastro`, payload);
+  }
+
+  login(payload: LoginPayload): Observable<AuthResponse> {
+    return this.http.post<AuthResponse>(`${this.authUrl}/login`, payload);
+  }
+}
