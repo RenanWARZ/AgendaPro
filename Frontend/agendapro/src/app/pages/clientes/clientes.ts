85ef3ac32a8de80b9fff02eda44c14ea82d6c746
RenanWARZ <renanpilan711@gmail.com>
2026-05-27 03:49:20 -0300

    Adicionando estilizações nas telas

--- Frontend/agendapro/src/app/pages/clientes/clientes.ts
+++ Frontend/agendapro/src/app/pages/clientes/clientes.ts
@@ -1,37 +1,48 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { ClienteService } from '../../../service/cliente.service';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { RouterLink, Router } from '@angular/router';
 
 @Component({
   selector: 'app-clientes',
+  standalone: true,
   imports: [CommonModule, RouterLink],
   templateUrl: './clientes.html',
   styleUrl: './clientes.css',
 })
+
 export class Clientes implements OnInit {
   clientes: any[] = [];
+  role = '';
 
-  constructor(private clienteService: ClienteService, private cd: ChangeDetectorRef) {}
+  constructor(
+    private clienteService: ClienteService,
+    private router: Router,
+    private cd: ChangeDetectorRef,
+  ) {}
 
   ngOnInit(): void {
+    if (typeof window !== 'undefined') {
+      this.role = localStorage.getItem('role') || '';
+    }
     this.listar();
-    this.cd.detectChanges();
   }
 
   listar() {
     this.clienteService.listar().subscribe({
       next: (res: any) => {
         console.log(res);
-
         this.clientes = res;
         this.cd.detectChanges();
       },
-
       error: (err) => {
         console.log(err);
       },
-
     });
+  }
+
+  sair() {
+    localStorage.clear();
+    this.router.navigate(['/login']);
   }
 }
